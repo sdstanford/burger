853e245037ad6063392e94afbc7e60808aff0ea8
Shannon Doepke <shannondoepke@Shannons-MacBook-Air.local>
2018-04-26 20:59:43 -0400

    Troubleshoot Heroku server issue

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,60 +1,60 @@
-var express = require("express");
+// var express = require("express");
 
-var router = express.Router();
+// var router = express.Router();
 
-router.get("/", function(req, res) {
-    connection.query("SELECT * FROM burgers;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
+// router.get("/", function(req, res) {
+//     connection.query("SELECT * FROM burgers;", function(err, data) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
 
-      var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+//       var hbsObject = {
+//             burger: data
+//         };
+//         console.log(hbsObject);
+//         res.render("index", hbsObject);
 
-    });
-  });
+//     });
+//   });
   
-  // Create a new burger
-  router.post("/burger", function(req, res) {
-    connection.query("INSERT INTO burgers (burger_name) VALUES ??", [req.params.newburger], function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
+//   // Create a new burger
+//   router.post("/burger", function(req, res) {
+//     connection.query("INSERT INTO burgers (burger_name) VALUES ??", [req.params.newburger], function(err, result) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
   
-      res.json({ id: result.insertId });
-    });
-  });
+//       res.json({ id: result.insertId });
+//     });
+//   });
   
-  //MYSQL ROUTES
-  //Get all Burgers
-  router.get("/burgers", function(req, res) {
-    connection.query("SELECT * FROM burgers;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
+//   //MYSQL ROUTES
+//   //Get all Burgers
+//   router.get("/burgers", function(req, res) {
+//     connection.query("SELECT * FROM burgers;", function(err, data) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
   
-      res.json(data);
-    });
-  });
+//       res.json(data);
+//     });
+//   });
   
-  //DEVOUR BURGER
-  router.delete("/burgers/:id", function(req, res) {
-    connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      }
-      else if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+//   //DEVOUR BURGER
+//   router.delete("/burgers/:id", function(req, res) {
+//     connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+//       if (err) {
+//         // If an error occurred, send a generic server failure
+//         return res.status(500).end();
+//       }
+//       else if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
   
-    });
-  });
+//     });
+//   });
 
-// Export routes for server.js to use.
-module.exports = router;
+// // Export routes for server.js to use.
+// module.exports = router;
